ef9619dc76c994520422d4f821bc940ad184e8e1
Akhil <akhil.madanan@nlindia.com>
2014-07-31 15:20:24 +0530

    jhgf

--- js/pushNotification.js
+++ js/pushNotification.js
@@ -116,7 +116,14 @@
     function IsDeviceRegResponseSuccess(result)
         {
 
-            alert(result.ApiResponse.Message);
+          
+            var resMessage = result.ApiResponse.Message;            
+           
+            if(resMessage == "Success")
+                {
+                   
+                       
+                }
 
 
         }
@@ -126,4 +133,36 @@
 
                 alert(1233);
 
+        }
+
+
+// iOS
+        function successHandler (result)
+        {
+            alert('result = ' + result);
+        }
+    function tokenHandler (result) 
+        {
+        // Your iOS push server needs to know the token before it can push to this device
+        // here is where you might want to send it the token for later use.
+        alert('device token = ' + result);
+            
+        }
+    function onNotificationAPN (event)
+        {
+            if ( event.alert )
+                {
+                navigator.notification.alert(event.alert);
+                }
+
+            if ( event.sound )
+                {
+                var snd = new Media(event.sound);
+                snd.play();
+                }
+
+            if ( event.badge )
+                {
+                pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+                }
         }
