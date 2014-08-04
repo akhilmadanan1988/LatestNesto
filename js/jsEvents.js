ae39464b1c5ec1e07d1a414f5a47d1c7c2c86e69
Akhil <akhil.madanan@nlindia.com>
2014-08-04 09:07:14 +0530

    ad
    asd

--- js/jsEvents.js
+++ js/jsEvents.js
@@ -8,6 +8,7 @@ $(function () {
 		
 		if (_thisid === 'home') {
 			$.getScript('js/login.js');
+            $.getScript('js/ pushNotification.js');           
 			localStorage.CardNumber = "";
 		}
 		else if (_thisid === 'forgotPassword')
